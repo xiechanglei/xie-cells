1ae4b6779fae68c6bd6c3db439b3b049937f6121
xiechanglei <xiechanglei@139.com>
2026-06-30 14:41:19 +0800

    测试

--- test/test-web/src/main/java/io/github/xiechanglei/cell/starter/test/TestJpaController.java
+++ test/test-web/src/main/java/io/github/xiechanglei/cell/starter/test/TestJpaController.java
@@ -4,6 +4,8 @@ import io.github.xiechanglei.cell.starter.jpa.auto.base.EntityClass;
 import io.github.xiechanglei.cell.starter.jpa.auto.task.DeleteById;
 import io.github.xiechanglei.cell.starter.jpa.auto.task.FindAll;
 import io.github.xiechanglei.cell.starter.jpa.auto.task.FindById;
+import io.github.xiechanglei.cell.starter.rbac.core.provide.RbacBean;
+import io.github.xiechanglei.cell.starter.rbac.core.provide.RbacPermission;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,20 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @EntityClass(value = TestTable.class, name = "用户")
+@RbacBean
 public class TestJpaController {
 
     @FindById(ignoreFields = "password")
     @RequestMapping("/test/jpa/findById")
+    @RbacPermission(name = "查询用户", code = "user:search", description = "查询用户信息")
     public void findById() {
     }
 
     @DeleteById
     @RequestMapping("/test/jpa/deleteById")
+    @RbacPermission(name = "删除用户", code = "user:delete", description = "删除用户信息")
     public void deleteById() {
     }
 
     @FindAll(ignoreFields = "password")
     @RequestMapping("/test/jpa/findAll")
+    @RbacPermission(name = "查询用户", code = "user:search", description = "查询用户信息")
     public void findAll() {
     }
 }
