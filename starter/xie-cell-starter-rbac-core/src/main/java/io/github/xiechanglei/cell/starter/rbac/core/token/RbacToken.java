9a6e4dae159515eeea0145fe813364ad7a998bcf
xiechanglei <xiechanglei@139.com>
2026-07-01 15:08:27 +0800

    分离配置参数

--- starter/xie-cell-starter-rbac-core/src/main/java/io/github/xiechanglei/cell/starter/rbac/core/token/RbacToken.java
+++ starter/xie-cell-starter-rbac-core/src/main/java/io/github/xiechanglei/cell/starter/rbac/core/token/RbacToken.java
@@ -1,5 +1,9 @@
 package io.github.xiechanglei.cell.starter.rbac.core.token;
 
+import io.github.xiechanglei.cell.common.lang.secret.AESHelper;
+
+import java.util.Date;
+
 /**
  * 类的详细说明
  *
@@ -7,4 +11,8 @@ package io.github.xiechanglei.cell.starter.rbac.core.token;
  * @date 2026/7/1
  */
 public class RbacToken {
+    static void main() throws Exception {
+        String encode = AESHelper.encode("" + new Date().getTime(), "cell-rbac", "cell-rbac");
+        System.out.println(encode.length());
+    }
 }
